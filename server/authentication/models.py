1abdc373633fbebf3f26ce916bc10625941bd066
JuanMarchant <juanmarchantdev@gmail.com>
2022-01-28 19:38:03 -0300

    Update CustomUser

--- server/authentication/models.py
+++ server/authentication/models.py
@@ -2,5 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(
-        max_length=150, unique=True)
+    email = models.EmailField(max_length=150, unique=True)
+    
+    REQUIRED_FIELD = ['username', 'password']
--- server/server/settings.py
+++ server/server/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'authentication',
 ]
 
+# Custom user model
+AUTH_USER_MODEL = 'authentication.CustomUser'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -125,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+DJANGO_COLORS = 'nocolor' 
